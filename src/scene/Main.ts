5885fcbb678695a1d97e2995d5f1b5201d62b5af
vidons <vidons@gmail.com>
2020-09-24 17:48:27 +0300

    Added background

--- src/scene/Main.ts
+++ src/scene/Main.ts
@@ -1,10 +1,15 @@
 class Main extends Phaser.Scene {
+    private background: Phaser.GameObjects.Sprite;
     constructor() {
         super("main");
     }
 
     create() {
-        let text: Phaser.GameObjects.Text = this.add.text(100, 100, "GAME should be here!");
+        this.background = new Phaser.GameObjects.Sprite(this, 0, 0, "background");
+        this.background.setPosition(this.cameras.main.width / 2, this.cameras.main.height / 2);
+        this.add.existing(this.background);
+        
+        let text: Phaser.GameObjects.Text = this.add.text(100, 100, "Game should be here!");
         text.setFontSize(36);
     }
 }
