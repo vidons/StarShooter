209f3d59306704611f535658ec9383e0e269889f
vidons <vidons@gmail.com>
2020-09-25 13:44:29 +0300

    fixed names

--- src/scene/Main.ts
+++ src/scene/Main.ts
@@ -1,6 +1,6 @@
 class Main extends Phaser.Scene {
     private background: Phaser.GameObjects.Sprite;
-    private eye: Phaser.GameObjects.Image;
+    private GalaxyClass: Phaser.GameObjects.Image;
 
     constructor() {
         super("main");
@@ -11,9 +11,10 @@ class Main extends Phaser.Scene {
         this.background.setPosition(this.cameras.main.width / 2, this.cameras.main.height / 2);
         this.add.existing(this.background);
 
-        this.eye = new Phaser.GameObjects.Image(this, 100, 100, "eye");
-        this.eye.setPosition(this.cameras.main.width / 4, this.cameras.main.height / 4);
-        this.add.existing(this.eye);
+        this.GalaxyClass = new Phaser.GameObjects.Image(this, 100, 100, "GalaxyClass");
+        this.GalaxyClass.setPosition(this.cameras.main.width / 4, this.cameras.main.height / 4);
+        this.add.existing(this.GalaxyClass);
+
         
         /*
         let text: Phaser.GameObjects.Text = this.add.text(100, 100, "Game should be here!");
